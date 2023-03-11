2551723596567338d310c5bd38a3a4206b61003b
Ferran <ferrancryptofx@gmail.com>
2023-03-11 13:06:08 +0100

    added the necessary files to start develop the pages and components

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+//Archivo que exportà todos loc componentes creados en estas paginas
+
+export { default as Home } from './Home';
+export { default as Profile } from './Profile';
+export { default as CreateProject } from './CreateProject';
+export { default as ProjectDetails } from './ProjectDetails';
