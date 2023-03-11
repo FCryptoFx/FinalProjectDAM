5e8ef3875407c08bbfe17cf28504a31b4902997a
Ferran <ferrancryptofx@gmail.com>
2023-03-11 12:19:47 +0100

    added folders and instruments necessary to start the development of the web · Some images to use in the page · /utils/index.js for some useful and repetitive functions · /constants/index.js for some link navegations

---
+++ client/src/constants/index.js
@@ -0,0 +1,39 @@
+//Este archivo no contiene lógica solamente links de navegación para que los podamos utilizar desde otro archivo
+
+import { createCampaign, dashboard, logout, payment, profile, withdraw } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'dashboard',
+    imgUrl: dashboard,
+    link: '/',
+  },
+  {
+    name: 'campaign',
+    imgUrl: createCampaign,
+    link: '/create-campaign',
+  },
+  {
+    name: 'payment',
+    imgUrl: payment,
+    link: '/',
+    disabled: true,
+  },
+  {
+    name: 'withdraw',
+    imgUrl: withdraw,
+    link: '/',
+    disabled: true,
+  },
+  {
+    name: 'profile',
+    imgUrl: profile,
+    link: '/profile',
+  },
+  {
+    name: 'logout',
+    imgUrl: logout,
+    link: '/',
+    disabled: true,
+  },
+];
---
+++ client/src/utils/index.js
@@ -0,0 +1,27 @@
+//Este archivo nos servirá para crear funciones que vamos a utilizar multiples veces en nuestra página
+
+//Función que calcula los días que quedan teniendo en cuanta el dia actual.
+export const daysLeft = (deadline) => {
+    const difference = new Date(deadline).getTime() - Date.now();
+    const remainingDays = difference / (1000 * 3600 * 24);
+  
+    return remainingDays.toFixed(0);
+  };
+  
+  //Función que calcula una barra de porcentage dependiendo del dinero aportado
+  export const calculateBarPercentage = (goal, raisedAmount) => {
+    const percentage = Math.round((raisedAmount * 100) / goal);
+  
+    return percentage;
+  };
+  
+  //Función que comprueba si la imagen es correcta
+  export const checkIfImage = (url, callback) => {
+    const img = new Image();
+    img.src = url;
+  
+    if (img.complete) callback(true);
+  
+    img.onload = () => callback(true);
+    img.onerror = () => callback(false);
+  };
