f7f1fd671ce023e15c8e7bac02e72c633edaa506
Ferran <ferrancryptofx@gmail.com>
2023-03-11 12:38:03 +0100

    tailwind.config.js and main.jsx conection corrected we forgot to import index.css in main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="relative sm:-8 p-4 bg-[#131318] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        Sidebar
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280] mx-auto sm:pr-5">
+        Navbar
+      </div>
+    </div>
   )
 }
 
