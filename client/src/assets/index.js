784d86b539475600b00f8a53eddc2c91e2f585be
Ferran <ferrancryptofx@gmail.com>
2023-03-13 18:44:43 +0100

    starting the Navbar

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -11,7 +11,7 @@ import search from './search.svg';
 import menu from './menu.svg';
 import money from './money.svg';
 import loader from './loader.svg';
-import thirdweb from './thirdweb.png';
+import profileButton from './profileButton.svg';
 
 export {
   tagType,
@@ -27,5 +27,5 @@ export {
   menu,
   money,
   loader,
-  thirdweb,
+  profileButton,
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+{/*import { useStateContext } from '../context';*/}
+import { CustomButton } from './';
+import { logo, menu, search, profileButton } from '../assets';
+import { navlinks } from '../constants';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const address ='0xabc';
+  //const { connect, address } = useStateContext();
+
   return (
-    <div>Navbar TEST</div>
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
+        <input type="text" placeholder="Search a project" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+            <img src={search} alt="search" className="w-[15px] h-[15px] object-contain"/>
+          </div>
+        </div>
+
+        {/* Este div solo aparecerá en dispositivos pequeños ya que estamos utilizando el "sm:flex"*/}
+        <div className="sm:flex hidden flex-row justify-end gap-4">
+        <CustomButton 
+          btnType="button"
+          title={address ? 'Create a project' : 'Connect'} //Comprobamos si existe una address para permitir crear una campaña
+          styles={address ? 'bg-[#4acd8d]' : 'bg-[#fad900]'} //Aplicamos estilos dependiendo si existe o no la address
+
+          //Callback function para que si es una address activa, podamos navegar al apartado 'create-project', sino debemos conectar una wallet.
+          handleClick={() => {
+            if(address) navigate('create-project')
+            else 'connect()'
+          }}
+        />
+
+        <Link to="/profile">
+          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+            <img src={profileButton} alt="user" className="w-[100%] h-[100%] object-contain" />
+          </div>
+        </Link>
+      </div>
+    </div>
   )
 }
 
