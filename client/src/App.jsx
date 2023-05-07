189e97a67aed36462a6dd49099bcbed97f9f479d
Ferran <ferrancryptofx@gmail.com>
2023-05-07 16:01:32 +0200

    added some style changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     <div>
       {address ? (
         <>
-          <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+          <div className="relative sm:-8 p-4 min-h-screen flex flex-row gradient-bg">
             <div className="sm:flex hidden mr-10 relative">
               <Sidebar />
             </div>
